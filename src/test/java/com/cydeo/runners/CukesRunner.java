49002cb6c1f83fd04564fdb57e937e680e89a477
onur <sevgilionur25@hotmail.com>
2022-06-22 15:20:14 +0300

    onur

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
         glue = "com/cydeo/step_definitions",
         dryRun = false,
         tags = "@onur",
-        publish = false
+        publish = true
 
 
 )
